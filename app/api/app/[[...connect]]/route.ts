62b0a0cdc7d70cd0269501f7e7f9698b844aa2fc
jasonblanchard <jas.blanchard@gmail.com>
2023-03-27 17:05:27 -0400

    app edge

--- app/api/app/[[...connect]]/route.ts
+++ app/api/app/[[...connect]]/route.ts
@@ -1,7 +1,7 @@
 import { next13JsApiRouter } from "@/adapters/connect-nextjs13-adapter";
 import routes from "@/connect";
 
-// export const runtime = "experimental-edge";
+export const runtime = "experimental-edge";
 
 const { POST } = next13JsApiRouter({
   routes,
