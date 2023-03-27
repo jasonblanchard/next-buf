f93b27c94930f4eba8f43b7d1be025d200641aac
jasonblanchard <jas.blanchard@gmail.com>
2023-03-27 19:54:25 -0400

    debugging

--- app/api/app/[[...connect]]/route.ts
+++ app/api/app/[[...connect]]/route.ts
@@ -3,9 +3,13 @@ import routes from "@/connect";
 
 // export const runtime = "experimental-edge";
 
-const { POST } = next13JsApiRouter({
-  routes,
-  prefix: "/api/app",
-});
+// const { POST } = next13JsApiRouter({
+//   routes,
+//   prefix: "/api/app",
+// });
 
-export { POST };
+// export { POST };
+
+export async function POST(request: Request) {
+  return new Response("Hello, Next.js!");
+}
